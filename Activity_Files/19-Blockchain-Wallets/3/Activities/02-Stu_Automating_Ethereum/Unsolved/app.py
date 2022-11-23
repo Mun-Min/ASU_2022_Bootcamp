ee6fb36007d97e9346ff10b785d13f2c14849f53
Mun-Min <minhasmunqiz@gmail.com>
2022-11-23 14:41:42 -0700

    added solved folders

--- Activity_Files/19-Blockchain-Wallets/3/Activities/02-Stu_Automating_Ethereum/Unsolved/app.py
+++ Activity_Files/19-Blockchain-Wallets/3/Activities/02-Stu_Automating_Ethereum/Unsolved/app.py
@@ -1,3 +1,4 @@
+# file path --> Activity_Files/19-Blockchain-Wallets/3/Activities/02-Stu_Automating_Ethereum/Unsolved
 # Streamlit Application
 
 # Imports
@@ -9,7 +10,8 @@ w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
 # @TODO
 # Update the imports for the functions coming from ethereum.py
 from ethereum import generate_account
-
+from ethereum import get_balance
+from ethereum import send_transaction
 
 ################################################################################
 # Streamlit Code
@@ -41,7 +43,8 @@ st.markdown("## Ethereum Account Balance:")
 
 # @TODO
 # Call the `get_balance` function and write the account balance to the screen
-
+ether_balance = get_balance(account.address) 
+st.write("Ether Balance", ether_balance)
 
 #######################################
 
@@ -53,7 +56,14 @@ st.markdown("## An Ethereum Transaction:")
 
 # @TODO
 # Create inputs for the receiver address and ether amount
-
+receiver = st.text_input('Enter receiver address')
+ether = st.text_input('Enter amount of ether to be sent')
 
 # @TODO
 # Create a button that calls the `send_transaction` function and returns the transaction hash
+if st.button('Send Transaction'): 
+    transaction_hash = send_transaction(account, receiver, ether)
+
+    st.markdown("## Ethereum Transaction Hash:")
+
+    st.write(transaction_hash)
--- Activity_Files/19-Blockchain-Wallets/3/Activities/02-Stu_Automating_Ethereum/Unsolved/ethereum.py
+++ Activity_Files/19-Blockchain-Wallets/3/Activities/02-Stu_Automating_Ethereum/Unsolved/ethereum.py
@@ -13,7 +13,7 @@ from web3 import middleware
 from web3.gas_strategies.time_based import medium_gas_price_strategy
 from web3 import Web3
 
-w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
+w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
 ################################################################################
 
 # Create a function called `generate_account` that automates the Ethereum
@@ -38,11 +38,36 @@ def generate_account():
 
 # @TODO
 # Create a function called `get_balance`, it should convert the wei balance of the account to ether, and returns the value of ether
+def get_balance(address): 
+    '''Function converts the wei balance to ether'''
 
+    wei_balance = w3.eth.get_balance(address)
 
+    ether = w3.fromWei(wei_balance, unit = 'ether')
 
-
-
+    return ether
 
 # @TODO
 # Create a function called `send_transaction` that creates a raw transaction, signs it, and sends it. Return the confirmation hash from the transaction.
+def send_transaction(account, receiver, ether): 
+    '''Function create a raw transaction, signs it, and sends it. 
+       returns confirmation hash from transaction'''
+
+    gas = w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
+
+    wei_value = w3.toWei(ether, unit = 'ether')
+
+    gas_estimate = w3.eth.estimateGas({"to": receiver, 
+                                       "from": account.address, 
+                                       "value" : wei_value})
+
+    raw_tx = {"to": receiver, 
+              "from": account.address,
+              "value" : wei_value, 
+              "gas":  gas_estimate, 
+              "gasPrice" : 20000000000, # w3.eth.generateGasPrice, 
+              "nonce" : w3.eth.getTransactionCount(account.address)}
+            
+    signed_tx = account.signTransaction(raw_tx)
+
+    return w3.eth.sendRawTransaction(signed_tx.rawTransaction)
