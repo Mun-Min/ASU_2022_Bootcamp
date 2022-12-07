1a8d5404bcb623faf8c32b4b220e630abc976485
Mun-Min <minhasmunqiz@gmail.com>
2022-12-07 14:24:11 -0700

    updated advanced solidity files day 2

--- Activity_Files/21-Advanced-Solidity/2/Activities/02-Stu_ERC20_Arcade_Token/Unsolved/ArcadeTokenERC20.sol
+++ Activity_Files/21-Advanced-Solidity/2/Activities/02-Stu_ERC20_Arcade_Token/Unsolved/ArcadeTokenERC20.sol
@@ -52,3 +52,27 @@ function mint(address recipient, uint amount) public onlyOwner {
 
 
 // Write the code for the import statements and ArcadeToken contract below:
+pragma solidity ^0.5.0; 
+
+// import libraries 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+
+// define contract 
+contract ArcadeToken is ERC20, ERC20Detailed { 
+    address payable owner; 
+
+    modifier onlyOwner {
+    require(msg.sender == owner, "You do not have permission to mint these tokens!");
+    _;
+    }
+
+    constructor(uint initial_supply) ERC20Detailed("ArcadeToken", "ARCD", 18) public {
+    owner = msg.sender;
+    _mint(owner, initial_supply);
+    }
+
+    function mint(address recipient, uint amount) public onlyOwner {
+    _mint(recipient, amount);
+    }
+}
